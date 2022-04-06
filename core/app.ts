7ea2e1ede50433b238b1c02a9e690816766729c6
Dinu <dinu@mediapark.com>
2022-04-06 20:49:13 +0300

    first commit

---
+++ core/app.ts
@@ -0,0 +1,13 @@
+import Koa from 'koa';
+
+function runApp() {
+    const app = new Koa();
+
+    app.use(async ctx => {
+        ctx.body = 'Message';
+    });
+
+    app.listen(3001, () => console.log('Server has been started at 3000'));
+}
+
+export default runApp();
