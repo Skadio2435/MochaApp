46d1d998cc8da2793fbcace0966958e77d3525bb
Dinu <dinu@mediapark.com>
2022-04-06 21:51:11 +0300

    add prettier to pre-commit

--- modules/customers/customer.service.spec.ts
+++ modules/customers/customer.service.spec.ts
@@ -2,6 +2,10 @@ import { expect } from 'chai';
 
 describe('test', () => {
     it('first test', () => {
-        expect(2).to.be.equal(2);
+      
+      
+        expect(2)
+        .to
+        .be.equal(2);
     });
 });
