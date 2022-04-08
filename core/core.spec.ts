5ab93b446e72d8d12dc77a395f6ae8ddedbf580a
Dinu <dinu@mediapark.com>
2022-04-08 14:22:26 +0300

    add 3 types of testings

---
+++ core/core.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('Core test', () => {
+    it('and first', () => {
+        expect(1).to.be.equal(1);
+    });
+});
---
+++ core/mocha.ts
@@ -0,0 +1,73 @@
+import Mocha from 'mocha';
+import fs from 'fs';
+import App from './app';
+
+function getDirectoryFilesWithAbsolutePath(directoryAbsolutePath: string): string[] {
+    return fs.readdirSync(directoryAbsolutePath).map(entryName => directoryAbsolutePath + '\\' + entryName);
+}
+
+class FileType {
+    filePath: string;
+
+    constructor(filePath: string) {
+        this.filePath = filePath;
+    }
+
+    isDirectory(): boolean {
+        return fs.lstatSync(this.filePath).isDirectory();
+    }
+
+    isTest(): boolean {
+        return this.filePath.slice(-8) === '.spec.ts';
+    }
+}
+
+function inspectFile(filePath: string): string[] {
+    const file = new FileType(filePath);
+
+    if (file.isDirectory()) return findTestFilesInDirectory(filePath);
+
+    if (file.isTest()) return [filePath];
+
+    return [];
+}
+
+function findTestFilesInDirectory(directory: string) {
+    const folderFiles = getDirectoryFilesWithAbsolutePath(directory);
+
+    return folderFiles.reduce((testFiles: string[], file: string) => testFiles.concat(inspectFile(file)), []);
+}
+
+function findTestFilesInDirectories(directories: string[]): string[] {
+    return directories.reduce(
+        (files: string[], directory: string) => files.concat(findTestFilesInDirectory(directory)),
+        []
+    );
+}
+
+async function runTestEnviroment() {
+    await App;
+
+    const mocha = new Mocha({
+        // growl: true,
+        // grep: 'Session',
+    });
+
+    const testFiles: string[] = findTestFilesInDirectories(['modules', 'core']);
+
+    testFiles.forEach(file => {
+        mocha.addFile(file);
+    });
+
+    const grep = process.argv[2];
+
+    if (grep) mocha.grep(grep);
+    mocha.timeout(240000);
+
+    mocha.run(failures => {
+        if (failures > 0) return process.exit(1);
+        return process.exit(0);
+    });
+}
+
+export default runTestEnviroment();
