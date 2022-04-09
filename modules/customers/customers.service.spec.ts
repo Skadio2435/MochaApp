ff6031a85141d7a74b73a19d59e81d254e67c23e
Dinu <dinu@mediapark.com>
2022-04-09 21:45:10 +0300

    set up mocha for vs code as bdd

--- modules/customers/customers.service.spec.ts
+++ modules/customers/customers.service.spec.ts
@@ -1,7 +1,22 @@
 import { expect } from 'chai';
 
-describe('testSuite', () => {
-    it('first test', () => {
-        expect(2).to.be.equal(2);
+describe('Test TDD', () => {
+    it('SHould do ', () => {
+        expect(1).equal(1);
+    });
+
+    it('second', () => {
+        expect(1).equal(1);
     });
 });
+
+// describe('testSuite', () => {
+//     it('first test', () => {
+//         expect(2).to.be.equal(2);
+//     });
+
+//     it('second test', () => {
+//         // todo disable ts for optimization
+//         expect(2).to.be.equal(2);
+//     });
+// });
--- modules/sessions/sessions.service.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import num from './session.service';
-
-describe('Session test', () => {
-    it('and first', () => {
-        expect(1).to.be.equal(1);
-        expect(num).to.be.equal(22);
-    });
-});
