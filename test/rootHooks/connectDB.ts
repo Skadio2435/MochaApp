753e475dc30b934dabaf019b4daefcae84a98612
Dinu <dinu@mediapark.com>
2022-04-10 17:57:25 +0300

    separate node env for mocha

--- test/rootHooks/connectDB.ts
+++ test/rootHooks/connectDB.ts
@@ -1,10 +1,7 @@
-import { NODE_ENVS } from '../../config/static';
 import db from '../../core/database';
 
 export const mochaHooks = {
     beforeAll: async function () {
-        process.env.NODE_ENV = NODE_ENVS.MOCHA;
-
         await db.setup();
     },
 };
--- test/rootHooks/runApp.ts
+++ test/rootHooks/runApp.ts
@@ -1,10 +1,7 @@
-import { NODE_ENVS } from '../../config/static';
 import App from '../../core/app';
 
 export const mochaHooks = {
     beforeAll: async function () {
-        process.env.NODE_ENV = NODE_ENVS.MOCHA;
-
         await App;
     },
 };
